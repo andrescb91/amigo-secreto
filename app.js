4d3c8209f312c45df0c2d4f1041c7f0a7ca9f8f2
AndresCadena <andres.cadena.b@hotmail.es>
2025-03-13 00:19:10 -0500

    Se agrego a app.js la logica para elegir de manera aleatoria el amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let NumeroAmigos=1;
+let listaAmigos=[];
+function agregarAmigo(){
+    let amigo=document.getElementById("amigo");
+    let lista=document.getElementById("listaAmigos");
+    lista.innerHTML+=`<li id="friend_${NumeroAmigos}">${amigo.value}</li>`;
+    listaAmigos.push(amigo.value);
+    NumeroAmigos++;
+}
+
+
+function sortearAmigo(){
+    let aleatorio=Math.trunc(Math.random()*listaAmigos.length);
+    console.log(aleatorio)
+    document.getElementById("resultado").innerHTML=`Tu amigo secreto es ${listaAmigos[aleatorio]}`
+}
+
+
